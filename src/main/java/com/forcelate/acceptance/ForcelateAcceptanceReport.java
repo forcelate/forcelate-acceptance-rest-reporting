8035bafc1b080bf4022a6a5fb0bb4cc3c3911849
Pavlo Pastushok <pavlo.pastushok@gmail.com>
2019-03-08 14:36:25 +0200

    Jenkinsfile test

--- src/main/java/com/forcelate/acceptance/ForcelateAcceptanceReport.java
+++ src/main/java/com/forcelate/acceptance/ForcelateAcceptanceReport.java
@@ -18,7 +18,7 @@ public final class ForcelateAcceptanceReport {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationBeans.class);
         ReportService reportService = context.getBean(ReportService.class);
         FreemarkerUtils freemarkerUtils = context.getBean(FreemarkerUtils.class);
-        Report report = reportService.formatReport(frameworkType);
+        Report report = reportSerice.formatReport(frameworkType);
         freemarkerUtils.generateReport(report);
     }
 }
